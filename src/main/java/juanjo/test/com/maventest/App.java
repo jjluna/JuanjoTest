8d541f95d508760b6c207b0fb5f988d59fd82e5f
jjluna <jjluna@gmail.com>
2020-01-20 16:44:48 -0500

    Add files via upload

---
+++ src/main/java/juanjo/test/com/maventest/App.java
@@ -0,0 +1,13 @@
+package juanjo.test.com.maventest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
